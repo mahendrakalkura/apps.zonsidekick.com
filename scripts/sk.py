c4333410a286386b7e6f14195799b773a653e588
Mahendra Kalkura <mahendrakalkura@gmail.com>
2014-08-20 20:53:28 +0530

    sk.py

---
+++ scripts/sk.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from furl import furl
+from simplejson import JSONDecodeError, loads
+
+from utilities import get_responses
+
+
+def get_suggested_keywords(original_keywords):
+    suggested_keywords = []
+    original_keywords = [
+        original_keyword.lower() for original_keyword in original_keywords
+    ]
+    for response in get_responses([
+        furl(
+            'http://completion.amazon.com/search/complete'
+        ).add({
+            'mkt': '1',
+            'q': original_keyword,
+            'search-alias': 'digital-text',
+            'xcat': '2',
+        }).url
+        for original_keyword in original_keywords
+    ]):
+        if not response:
+            continue
+        contents = ''
+        try:
+            contents = loads(response.text)
+        except JSONDecodeError:
+            pass
+        if not contents:
+            continue
+        for keyword in contents[1]:
+            if not ' ' in keyword:
+                continue
+            words = keyword.split(' ')
+            if all(word in original_keywords for word in words):
+                if not keyword in suggested_keywords:
+                    suggested_keywords.append(keyword)
+    return suggested_keywords
