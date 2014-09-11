4a5ecf5846b1b03a301cffd0a272f65c9eb6928a
Mahendra Kalkura <mahendrakalkura@gmail.com>
2014-09-11 21:59:43 +0530

    cleanup

--- files/8.sql
+++ files/8.sql
@@ -1,1 +1,3 @@
-ALTER TABLE `tools_ps_trends` ADD `popularity` DECIMAL(9,2) NOT NULL AFTER `date_and_time`;
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `tools_ps_trends` ADD `popularity` DECIMAL(9,2) NOT NULL AFTER `keywords`;
