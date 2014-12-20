2544631ce4392827bf7e02931637e1e94bb752e4
Mahendra Kalkura <mahendrakalkura+bitbucket.org@gmail.com>
2014-12-20 22:23:19 +0530

    pivotaltracker_84972234

--- scripts/assets.py
+++ scripts/assets.py
@@ -6,6 +6,8 @@ from webassets import Bundle, Environment
 from webassets.script import CommandLineEnvironment
 from webassets.script import CommandLineEnvironment
 
+from utilities import variables
+
 logger = getLogger('webassets')
 logger.setLevel(DEBUG)
 logger.addHandler(StreamHandler())
@@ -13,7 +15,7 @@ logger.addHandler(StreamHandler())
 environment = Environment()
 
 environment.cache = False
-environment.debug = False
+environment.debug = variables['application']['debug']
 environment.directory = '../'
 environment.manifest = False
 environment.url = '../'
@@ -34,7 +36,7 @@ environment.register('javascripts', Bundle(
     'vendor/pixeladmin/html/assets/javascripts/pixel-admin.min.js',
     'vendor/jquery-zclip/jquery.zclip.js',
     'javascripts/all.js',
-    filters='rjsmin',
+    filters=None if environment.debug else 'rjsmin',
     output='assets/javascripts.js',
 ))
 environment.register('stylesheets', Bundle(
@@ -45,7 +47,7 @@ environment.register('stylesheets', Bundle(
     'vendor/pixeladmin/html/assets/stylesheets/rtl.css',
     'vendor/pixeladmin/html/assets/stylesheets/themes.css',
     'stylesheets/all.css',
-    filters='cssmin,cssrewrite',
+    filters='cssrewrite' if environment.debug else 'cssmin,cssrewrite',
     output='assets/stylesheets.css',
 ))
 
