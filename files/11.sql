19594e6215341c9e6c862ce34c0f355806ccafef
Mahendra Kalkura <mahendrakalkura+bitbucket.org@gmail.com>
2014-12-25 21:40:24 +0530

    MySQL

--- files/11.sql
+++ files/11.sql
@@ -7,4 +7,12 @@ RENAME TABLE `tools_kns_requests` TO `apps_keyword_analyzer_reports`;
 RENAME TABLE `tools_ps_books` TO `apps_popular_searches_books`;
 RENAME TABLE `tools_ps_trends` TO `apps_popular_searches_trends`;
 
+ALTER TABLE `apps_keyword_analyzer_keywords` DROP FOREIGN KEY `tools_keywords_request_id`;
+ALTER TABLE `apps_keyword_analyzer_keywords` DROP KEY `request_id`;
+
 ALTER TABLE `apps_keyword_analyzer_keywords` CHANGE `request_id` `report_id` INT(11) NOT NULL;
+
+SET FOREIGN_KEY_CHECKS = 0;
+ALTER TABLE `apps_keyword_analyzer_keywords` ADD KEY `report_id` (`report_id`);
+ALTER TABLE `apps_keyword_analyzer_keywords` ADD CONSTRAINT `apps_keyword_analyzer_keywords_report_id` FOREIGN KEY (`report_id`) REFERENCES `apps_keyword_analyzer_reports` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+SET FOREIGN_KEY_CHECKS = 1;
